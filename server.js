7f399c2c34489709f6395d403284456c1ebdd731
saumy85 <120151436+saumy85@users.noreply.github.com>
2024-04-13 02:15:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,17 +29,15 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use(express.static(path.join(__dirname, "../frontend/e-commerce/build")));
+
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
-});
 
+app.use(express.static(path.join(__dirname, "../frontend/e-commerce/build")));
 //PORT
 const PORT = process.env.PORT || 8080;
 
