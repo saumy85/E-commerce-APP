654bbcdb93fe7b6ed09119889ba41facac00431d
saumy85 <120151436+saumy85@users.noreply.github.com>
2024-04-13 02:30:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "./client/build")));
 //rest object
-const app = express();
 
 //middelwares
 app.use(cors());
